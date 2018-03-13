50d91c3e9623f7140154400a162996e94a7535a3
Imad E <enneiymy.i@gmail.com>
2018-03-13 18:58:14 +0100

    Adding project files

---
+++ ArchiProj/Assets/Scripts/ProjectileAnim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileAnim : MonoBehaviour {
+
+	public float speed;
+
+	private Transform player;
+	private Vector2 target;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		//target = new Vector2 (player.position.x-150, player.position.y);
+		target = new Vector2 (player.position.x, player.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.MoveTowards (transform.position, target, speed * Time.deltaTime);
+
+		if (transform.position.x == target.x && transform.position.y == target.y) {
+			DestroyProjectile ();
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D o){
+		if (o.CompareTag ("Player")) {
+			DestroyProjectile ();
+		}
+	}
+
+	void DestroyProjectile (){
+		Destroy (gameObject);
+	}
+}
---
+++ ArchiProj/Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour {
+
+	private float wait; // time between shots;
+	public float start; // start time between shots;
+	public GameObject projectile;
+	private Transform player;
+
+	// Use this for initialization
+	void Start () {
+
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		wait = start;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (wait <= 0) {
+			Instantiate (projectile, transform.position, Quaternion.identity);
+			wait = start;
+		} else {
+			wait -= Time.deltaTime;
+		}
+	}
+}
