030b94b241af28396c95acf4ee4411d766344981
Yassir Karroum <ukarroum17@gmail.com>
2018-03-18 14:36:43 +0100

    Fix projectile issue

--- ArchiProj/Assets/Scripts/ProjectileAnim.cs
+++ ArchiProj/Assets/Scripts/ProjectileAnim.cs
@@ -7,14 +7,17 @@ public class ProjectileAnim : MonoBehaviour {
 	public float speed;
 
 	private Transform player;
+    private Transform leftBC;
+
 	private Vector2 target;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
+        leftBC = GameObject.FindGameObjectWithTag("LeftBC").transform;
 
 		//target = new Vector2 (player.position.x-150, player.position.y);
-		target = new Vector2 (player.position.x, player.position.y);
+		target = new Vector2 (leftBC.position.x, player.position.y);
 	}
 	
 	// Update is called once per frame
@@ -30,7 +33,9 @@ public class ProjectileAnim : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D o){
 		if (o.CompareTag ("Player")) {
 			DestroyProjectile ();
-		}
+            var exp = GetComponent<ParticleSystem>();
+            exp.Play();
+        }
 	}
 
 	void DestroyProjectile (){
