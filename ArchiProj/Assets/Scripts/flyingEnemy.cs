cc6a7db847a0290dc61776934340d10696ee822b
Yassir Karroum <ukarroum17@gmail.com>
2018-03-21 18:57:30 +0100

    Add a flying enemy

---
+++ ArchiProj/Assets/Scripts/flyingEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flyingEnemy : MonoBehaviour {
+
+    private float wait;
+    public float start;
+    public GameObject enemy;
+
+    // Use this for initialization
+    void Start ()
+    {
+        wait = start;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (wait <= 0)
+        {
+            Instantiate(enemy, new Vector2(Random.Range(GameObject.FindGameObjectWithTag("LeftBC").transform.position.x, GameObject.FindGameObjectWithTag("RightBC").transform.position.x), transform.position.y), Quaternion.identity);
+            wait = start;
+        }
+        else
+        {
+            wait -= Time.deltaTime;
+        }
+    }
+}
