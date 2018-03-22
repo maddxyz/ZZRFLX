568cd44eead709baa28d3ac6635119d8367d549f
Imad E <enneiymy.i@gmail.com>
2018-03-22 19:12:03 +0100

    Added enemy spaceship

---
+++ ArchiProj/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	[System.Serializable]
+	public class PlayerStats {
+		public int Health = 100;
+	}
+
+	public PlayerStats playerStats = new PlayerStats();
+
+	void Update () {
+		
+	}
+
+	/*public void DamagePlayer (int damage) {
+		playerStats.Health -= damage;
+		if (playerStats.Health <= 0) {
+			GameMaster.KillPlayer(this);
+		}
+	}*/
+
+}
